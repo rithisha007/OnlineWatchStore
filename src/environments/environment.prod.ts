9c9a895e29fa40a8be5c1c26b1cc80f525d54da4
rithisha <rithisha365@gmail.com>
2024-01-03 17:45:37 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    UserDetails: 'http://localhost:3000/signup',
+    CartItems: 'http://localhost:3000/cartItems',
+    ProductDetails: 'http://localhost:3000/products',
+    BaseUrl: 'http://localhost:3000',
+    LoggedInUserKey: 'loggedInUser',
+    AdminEmail: 'Admin007@gmail.com',
+    AdminPassword:'admin007'
+  };
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    UserDetails: 'http://localhost:3000/signup',
+    CartItems: 'http://localhost:3000/cartItems',
+    ProductDetails: 'http://localhost:3000/products',
+    BaseUrl: 'http://localhost:3000',
+    LoggedInUserKey: 'loggedInUser',
+    AdminEmail: 'Admin007@gmail.com',
+    AdminPassword:'admin007'
+  };
