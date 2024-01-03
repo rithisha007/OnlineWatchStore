6baf739e9d4d1fc70c0a61cbd5d1e21382097633
rithisha007 <130377987+rithisha007@users.noreply.github.com>
2024-01-03 17:25:03 +0530

    Add files via upload

---
+++ src/app/Couples/Couples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Couples',
+  templateUrl: './Couples.component.html',
+  styleUrls: ['./Couples.component.css']
+})
+export class CouplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
